b8ee43afaa63c4cfec67a00b2e72b8fd8f06f52d
minji <flowing_mj@naver.com>
2019-03-12 15:30:55 +0900

    [ui] create parallax scrolling

--- js/common.js
+++ js/common.js
@@ -6,3 +6,31 @@ var uiNoticePopup = (function() {
     })
   })
 })();
+
+var mobileCheck = (function() {
+  var checkMobile = function () {
+    var mobileOS = {
+      Android: function () {
+        return navigator.userAgent.match(/Android/i) ? true : false;
+      },
+      BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i) ? true : false;
+      },
+      iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false;
+      },
+      Windows: function () {
+        return navigator.userAgent.match(/Windows Phone|IEMobile/i) ? true : false;
+      },
+      any: function () {
+        return (mobileOS.Android() || mobileOS.BlackBerry() || mobileOS.iOS() || mobileOS.Windows());
+      }
+    };
+
+    return mobileOS.any();
+  };
+  
+  return function() {
+    if ( checkMobile() ) alert("현재 페이지는 PC 버전으로 작업되었습니다:)");
+  };
+})()
